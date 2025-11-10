853da0b3a8da6a78322a2a64c03c6113c867e20e
Zsombor06 <simon.zsombor06@gmail.com>
2025-11-10 17:16:01 +0100

    adminBackEnd_Termék_v1

---
+++ backend/index.php
@@ -0,0 +1,136 @@
+<?php
+include "./sql_fuggvenyek.php";
+$metodus=$_SERVER["REQUEST_METHOD"];
+$uri=parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH);
+$uri=explode("/",$uri);
+$bodyAdatok=json_decode(file_get_contents("php://input"),true);
+switch(end($uri)){
+    case "felhasznaloAdatok":
+        if($metodus!="GET"){
+            return http_response_code(405);
+        }
+        $getFelhasznaloAdatSQL="SELECT felhasználónév,rangId FROM felhasználó WHERE=?";
+        $getFelhasznaloAdat=adatokLekerese($getFelhasznaloAdatSQL,"s",[$_GET["felhasznalo"]]);
+        echo json_encode($getFelhasznaloAdat,JSON_UNESCAPED_UNICODE);
+        return http_response_code(200);
+    case "felhasznaloNevek":
+        if($metodus!="GET"){
+            return http_response_code(405);
+        }
+        $getFelhasznalonevSQL="SELECT felhasználónév FROM felhasználó";
+        $getFelhasznalonev=adatokLekerese($getFelhasznalonevSQL);
+        echo json_encode($getFelhasznalonev,JSON_UNESCAPED_UNICODE);
+        return http_response_code(200);
+    case "ujFelhasznalo":
+    if($metodus!="POST"){
+        return http_response_code(405);
+    }
+    if(empty($bodyAdatok["felhasználónév"]) || empty($bodyAdatok["jelszó"]) || empty($bodyAdatok["email"]) ||empty($bodyAdatok["számlázásicím"]) ||empty($bodyAdatok["rangId"]))
+    {
+        echo json_encode(["valasz"=>"Hiányzó adat!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(400);
+    }
+    $ujFelhasznaloSQL="INSERT INTO felhasználó (felhasználónév, jelszó, email, számlázásicím, rangId) VALUES (?,?,?,?,?)";
+    $ujFelhasznalo=adatokValtoztatasa($ujFelhasznaloSQL,"ssssi",[$bodyAdatok["felhasználónév"],$bodyAdatok["jelszó"],$bodyAdatok["email"],$bodyAdatok["számlázásicím"],$bodyAdatok["rangId"]]);
+    if($ujFelhasznalo){
+        echo json_encode(["valasz"=>"Sikeres feltöltés!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(201);
+    }
+    echo json_encode(["valasz"=>"Sikertelen feltöltés!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(400);
+    
+    case "modositFelhasznalo":
+    if($metodus!="PUT"){
+        return http_response_code(405);
+    }
+    $modositFelhasznaloSQL="UPDATE felhasználó SET jelszó=?, email=?, számlázásicím=?,rangId=? WHERE felhasználónév=?";
+    $modositFelhasznalo=adatokValtoztatasa($modositFelhasznaloSQL,"sssis",[$bodyAdatok["jelszó"],$bodyAdatok["email"],$bodyAdatok["számlázásicím"],$bodyAdatok["rangId"],$bodyAdatok["felhasználónév"]]);
+    if($modositFelhasznalo){
+        echo json_encode(["valasz"=>"Sikeres módosítás!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(201);
+    }
+    echo json_encode(["valasz"=>"Sikertelen módosítás!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(400);
+    case "felhasznaloTorles":
+        if($metodus!="DELETE"){
+            return http_response_code(405);
+        }
+        $cimekTorleseSQL="DELETE FROM szállításicímek WHERE felhasználó=?";
+        $cimekTorlese=adatokValtoztatasa($cimekTorleseSQL,"s",[$bodyAdatok["felhasználónév"]]);
+        $felhasznaloTorlesSQL="DELETE FROM felhasználó WHERE felhasználónév=?";
+        $felhasznaloTorles=adatokValtoztatasa($felhasznaloTorlesSQL,"s",[$bodyAdatok["felhasználónév"]]);
+        if($cimekTorlese && $felhasznaloTorles){
+            echo json_encode(["valasz"=>"Sikeres törlés!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(201);
+    }
+    echo json_encode(["valasz"=>"Sikertelen törlés!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(400);
+    case "kategoriakNeve":
+        if($metodus!="GET"){
+            return http_response_code(405);
+        }
+        $getKategoriakSQL="SELECT * from kategória";
+        $getKategoriak=adatokLekerese($getKategoriakSQL);
+        echo json_encode($getKategoriak,JSON_UNESCAPED_UNICODE);
+        return http_response_code(200);
+    case "learazasokMerteke":
+        if($metodus!="GET"){
+            return http_response_code(405);
+        }
+        $getLearazasokSQL="SELECT * from leárazás";
+        $getLearazasok=adatokLekerese($getLearazasokSQL);
+        echo json_encode($getLearazasok,JSON_UNESCAPED_UNICODE);
+        return http_response_code(200);
+    case "ujTermek":
+        if($metodus!="POST"){
+            return http_response_code(405);
+        }
+        if(empty($bodyAdatok["név"]) ||empty($bodyAdatok["névEn"]) ||empty($bodyAdatok["leírás"]) ||empty($bodyAdatok["leírásEn"]) ||empty($bodyAdatok["ár"]) ||empty($bodyAdatok["kategóriaId"]) || empty($bodyAdatok["készlet"]) ||empty($bodyAdatok["leárazásId"]) ||empty($bodyAdatok["kép"])){
+            echo json_encode(["valasz"=>"Hiányzó adat"]);
+        }
+        $postUjTermekSQL="INSERT INTO `termék`( ?,?,?,?,?,?,?,?,?)";
+        $postUjTermek=adatokValtoztatasa($postUjTermekSQL,"ssssiiiib",[$bodyAdatok["név"],$bodyAdatok["névEn"],$bodyAdatok["leírás"],$bodyAdatok["leírásEn"],$bodyAdatok["ár"],$bodyAdatok["kategóriaId"],$bodyAdatok["készlet"],$bodyAdatok["leárazásId"],$bodyAdatok["kép"]]);
+        if($postUjTermek){
+            echo json_encode(["valasz"=>"Sikeres feltöltés"],JSON_UNESCAPED_UNICODE);
+            return http_response_code(201);
+        }
+         echo json_encode(["valasz"=>"Sikertelen feltöltés"],JSON_UNESCAPED_UNICODE);
+            return http_response_code(400);
+    case "termekNeve":
+        if($metodus!="GET"){
+            return http_response_code(405);
+        }
+        $gettermekNevSQL="SELECT id,név from termék";
+        $gettermekNev=adatokLekerese($gettermekNevSQL);
+        echo json_encode($gettermekNev,JSON_UNESCAPED_UNICODE);
+        return http_response_code(200);
+    case "modositTermek":
+        if($metodus!="PUT"){
+            return http_response_code(405);
+        }
+        if(empty($bodyAdatok["név"]) ||empty($bodyAdatok["névEn"]) ||empty($bodyAdatok["leírás"]) ||empty($bodyAdatok["leírásEn"]) ||empty($bodyAdatok["ár"]) ||empty($bodyAdatok["kategóriaId"]) || empty($bodyAdatok["készlet"]) ||empty($bodyAdatok["leárazásId"]) ||empty($bodyAdatok["kép"])){
+            echo json_encode(["valasz"=>"Hiányzó adat"]);
+        }
+        $putTermekModositSQL="UPDATE `termék` SET `név`=?,`névEn`=?,`leírás`=?,`leírásEn`=?,`ár`=?,`kategóriaId`=?,`készlet`=?,`leárazásid`=?,`kép`=? WHERE `id`=?";
+        $putTermekModosit=adatokValtoztatasa($putTermekModositSQL,"ssssiiiibi",[$bodyAdatok["név"],$bodyAdatok["névEn"],$bodyAdatok["leírás"],$bodyAdatok["leírásEn"],$bodyAdatok["ár"],$bodyAdatok["kategóriaId"],$bodyAdatok["készlet"],$bodyAdatok["leárazásId"],$bodyAdatok["kép"],$bodyAdatok["id"]]);
+        if($putTermekModosit){
+            echo json_encode(["valasz"=>"Sikeres módosítés"],JSON_UNESCAPED_UNICODE);
+            return http_response_code(201);
+        }
+         echo json_encode(["valasz"=>"Sikertelen módosítés"],JSON_UNESCAPED_UNICODE);
+            return http_response_code(400);
+    case "termekTorles":
+        if($metodus!="DELETE"){
+            return http_response_code(405);
+        }
+        $termekTorlesSQL="DELETE FROM termék WHERE id=?";
+        $termekTorles=adatokValtoztatasa($termekTorlesSQL,"i",[$bodyAdatok["termekId"]]);
+        if($termekTorles){
+            echo json_encode(["valasz"=>"Sikeres törlés!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(201);
+        }
+        echo json_encode(["valasz"=>"Sikertelen törlés!"],JSON_UNESCAPED_UNICODE);
+        return http_response_code(400);
+
+}
+?>
