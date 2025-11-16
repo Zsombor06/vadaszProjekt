e7e1bcbae5bdbc94c1f9017ddd116dae54329570
CsananaBanana <csanadbukkosi2007@gmail.com>
2025-11-16 17:04:27 +0100

    navbar igazítás, javascriptek javítása, bejelentkezési oldal

---
+++ frontend/bejelentkezesiStatus.js
@@ -0,0 +1,22 @@
+export const bejelentkezesiStatus = async () => {
+        try {
+            let httpValasz = await fetch(""); //be van e jelentkezve?
+            if (httpValasz.ok) {
+                let httpAdat = await httpValasz.json();
+                if (httpAdat == "") { //nincs bejelentkezve 
+                    return 0;
+                } else { //be van jelentkezve
+                    switch (httpAdat["rangId"]) {
+                        case 1: //rangId 1 az admin
+                            return 1;
+                        case 2: //rangId 2 a dolgozó
+                            return 2;
+                        default: //rangId 3 a regisztrált felhasználó
+                            return 3;
+                    }
+                }
+            }
+        } catch (error) {
+            return console.log(error);
+        }
+    }
