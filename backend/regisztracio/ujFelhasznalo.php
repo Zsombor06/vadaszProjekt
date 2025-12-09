c95bd574c87daf9377abe4e955fb9d17c6536e85
Levente4523 <hardilevi12@gmail.com>
2025-12-09 17:31:29 +0100

    Regisztracio osszekotes + modositasok

--- backend/regisztracio/ujFelhasznalo.php
+++ backend/regisztracio/ujFelhasznalo.php
@@ -5,21 +5,27 @@ header('Content-Type: application/json');
 $data = json_decode(file_get_contents("php://input"));
 
 // mezők ellenőrzése
-if (!isset($data->felhasznalonev) || 
-    !isset($data->jelszo) || 
+if (
+    !isset($data->felhasznalonev) ||
+    !isset($data->jelszo) ||
     !isset($data->email) ||
-    !isset($data->szamlazasiCim) ||
+    
+    !isset($data->szamlazasi_orszag) ||
+    !isset($data->szamlazasi_iranyitoszam) ||
+    !isset($data->szamlazasi_varos) ||
+    !isset($data->szamlazasi_utca) ||
+
     !isset($data->orszag) ||
     !isset($data->iranyitoszam) ||
     !isset($data->varos) ||
-    !isset($data->utca)) {
-
+    !isset($data->utca)
+) {
     http_response_code(400);
-    echo json_encode(["hiba" => "Minden mező kitöltése kötelező"]);
+    echo json_encode(["hiba" => "Minden mező kitöltése kötelező!"]);
     exit;
 }
 
-// felhasználónév foglaltság
+// felhasználónév ellenőrzése
 $stmt = $pdo->prepare("SELECT felhasznalonev FROM felhasznalo WHERE felhasznalonev = ?");
 $stmt->execute([$data->felhasznalonev]);
 
@@ -29,7 +35,7 @@ if ($stmt->fetch()) {
     exit;
 }
 
-//email foglaltság
+// email ellenőrzése
 $stmt = $pdo->prepare("SELECT email FROM felhasznalo WHERE email = ?");
 $stmt->execute([$data->email]);
 
@@ -39,20 +45,23 @@ if ($stmt->fetch()) {
     exit;
 }
 
-
-// jelszó hash-elése
+// jelszó hash
 $hashedPassword = password_hash($data->jelszo, PASSWORD_DEFAULT);
 
 // felhasználó mentése
 $stmt = $pdo->prepare("
-    INSERT INTO felhasznalo (felhasznalonev, jelszo, email, szamlazasicim, rangId)
-    VALUES (?, ?, ?, ?, 3)
+    INSERT INTO felhasznalo 
+    (felhasznalonev, jelszo, email, szamlazasi_orszag, szamlazasi_iranyitoszam, szamlazasi_varos, szamlazasi_utca, rangId)
+    VALUES (?, ?, ?, ?, ?, ?, ?, 3)
 ");
 $stmt->execute([
     $data->felhasznalonev,
     $hashedPassword,
     $data->email,
-    $data->szamlazasiCim
+    $data->szamlazasi_orszag,
+    $data->szamlazasi_iranyitoszam,
+    $data->szamlazasi_varos,
+    $data->szamlazasi_utca
 ]);
 
 // szállítási cím mentése
@@ -68,6 +77,4 @@ $stmt->execute([
     $data->utca
 ]);
 
-echo json_encode([
-    "uzenet" => "Felhasználó sikeresen létrehozva!"
-]);
+echo json_encode(["uzenet" => "Felhasználó sikeresen létrehozva!"]);
--- frontend/regisztracio/regisztracio.js
+++ frontend/regisztracio/regisztracio.js
@@ -30,34 +30,67 @@ document.getElementById("BejGomb").addEventListener("click", function () {
     }
 });
 
+
 const Regisztracio = async () => {
-    try {
+     try {
         let fnev = document.getElementById("FnevIn").value;
         let jelszo = document.getElementById("FjelszoIn").value;
         let jelszoUjra = document.getElementById("FjelszoUjraIn").value;
         let email = document.getElementById("FemailIn").value;
-        let szamla = document.getElementById("FszamlaIn").value;
-        let lakcim = document.getElementById("FlakIn").value;
+
+        // számlázási cím
+        let szaml_or = document.getElementById("SzamlOrszag").value;
+        let szaml_ir = document.getElementById("SzamlIr").value;
+        let szaml_varos = document.getElementById("SzamlVaros").value;
+        let szaml_utca = document.getElementById("SzamlUtca").value;
+
+        // szállítási cím
+        let orszag = document.getElementById("FlakOrszag").value;
+        let irsz = document.getElementById("FlakIr").value;
+        let varos = document.getElementById("FlakVaros").value;
+        let utca = document.getElementById("FlakUtca").value;
+
         if (jelszo !== jelszoUjra) {
             alert("A jelszavak nem egyeznek!");
             return;
         }
-        //felhasználó név ellenőrzése (van e már ilyen)
-        //email ellenőrzése (van e már ilyen)
-        
-        // Küldés a szervernek
+
         let regAdatok = {
-            "felhasznalonev": fnev,
-            "jelszo": jelszo,
-            "email": email,
-            "szamla": szamla,
-            "lakcim": lakcim
+            felhasznalonev: fnev,
+            jelszo: jelszo,
+            email: email,
+
+            // számlázási cím
+            szamlazasi_orszag: szaml_or,
+            szamlazasi_iranyitoszam: szaml_ir,
+            szamlazasi_varos: szaml_varos,
+            szamlazasi_utca: szaml_utca,
+
+            // szállítási cím
+            orszag: orszag,
+            iranyitoszam: irsz,
+            varos: varos,
+            utca: utca
         };
-        let httpValasz = await fetch(`../../backend/regisztracio/index.php/regisztracio`, { //??
-            method: 'POST',
-            body: JSON.stringify(regAdatok),
+
+        let response = await fetch("../../backend/regisztracio/ujFelhasznalo.php", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(regAdatok)
         });
-    } catch (error) {
-        console.error();
+
+        let adat = await response.json();
+
+        if (response.ok) {
+            alert("Sikeres regisztráció!");
+        } else {
+            alert(adat.hiba);
+        }
+
+    } catch (err) {
+        console.error(err);
+        alert("Hálózati hiba!");
     }
 }
+
+document.getElementById("RegGomb").addEventListener("click", Regisztracio);
