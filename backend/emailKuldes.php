163da0eb0b09b385c5e93bb68aeaa9ebe6da0c79
CsananaBanana <csanadbukkosi2007@gmail.com>
2026-04-23 10:50:21 +0200

    Email küldés (phpmailer, .env hozzáadása szükséges)

---
+++ backend/emailKuldes.php
@@ -0,0 +1,35 @@
+<?php
+require '../vendor/autoload.php';
+require 'config.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+function emailKuldes($cimzettEmail, $cimzettNev, $targy, $uzenet) {
+    
+    $mail = new PHPMailer(true);
+    $mail->SMTPDebug = 2;
+
+    $mail->isSMTP();
+    $mail->CharSet = 'UTF-8';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['EMAIL_USERNAME'];
+    $mail->Password = $_ENV['EMAIL_PASSWORD'];
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom($_ENV['EMAIL_USERNAME'], 'Arany Agancs Webshop');
+    $mail->addAddress($cimzettEmail, $cimzettNev);
+
+    $mail->isHTML(true);
+    $mail->Subject = $targy;
+    $mail->Body = $uzenet;
+
+    try {
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return $mail->ErrorInfo;
+    }
+}
